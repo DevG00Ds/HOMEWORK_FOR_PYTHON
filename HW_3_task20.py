75d4c1ffe53d2a4d6cb549222392426209d3f32b
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-04-18 18:32:01 +0300

    finish task20

--- HW_3_task20.py
+++ HW_3_task20.py
@@ -23,11 +23,39 @@
 # Вывод: 12
 
 
+eng = 'qwertyuiopasdfghjklzxcvbnm'
+rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
 
+eng_list = {1: 'A, E, I, O, U, L, N, S, T, R',
+            2: 'D, G',
+            3: 'B, C, M, P',
+            4: 'F, H, V, W, Y',
+            5: ' K',
+            8: ' J, X',
+            10: ' Q, Z'}
 
+rus_list = {1: 'А, В, Е, И, Н, О, Р, С, Т',
+            2: 'Д, К, Л, М, П, У',
+            3: 'Б, Г, Ё, Ь, Я',
+            4: 'Й, Ы',
+            5: 'Ж, З, Х, Ц, Ч',
+            8: 'Ш, Э, Ю ',
+            10: 'Ф, Щ, Ъ'}
 
+word = input('Введите слово : ')
 
-
-
-
-
+if word[0].lower() in eng:
+    summa = 0
+    for letters in word:
+        for key, value in eng_list.items():
+            if letters.upper() in value:
+                summa += key
+    print(f'Сумма Английских букв = {summa}')
+else:
+    if word[0].lower() in rus:
+        summa = 0
+        for letters in word:
+            for key, value in rus_list.items():
+                if letters.upper() in value:
+                    summa += key
+    print(f'Сумма Русских букв = {summa}')
